7dfbcbe1676f315fadb92061d4aec46aa3a26f43
Bhavan Kumar V <bhavankumarcse2020@.com>
2024-06-03 14:39:38 +0530

    user repo test added

--- HotelBookingSystemSolution/APITest/UserRepoTest.cs
+++ HotelBookingSystemSolution/APITest/UserRepoTest.cs
@@ -1,13 +1,16 @@
 using HotelBookingSystemAPI;
 using HotelBookingSystemAPI.Context;
+using HotelBookingSystemAPI.Exceptions.User;
 using HotelBookingSystemAPI.Models;
 using HotelBookingSystemAPI.Repository;
 using HotelBookingSystemAPI.Repository.Interfaces;
 using HotelBookingSystemAPI.Services;
+using HotelBookingSystemAPI.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Moq;
 using RoleBasedAuthenticationAPI.Services.Interfaces;
+using System.Security.Cryptography;
 
 namespace APITest
 {
@@ -15,24 +18,127 @@ namespace APITest
     {
         private HotelBookingSystemContext _context;
         private IRepository<int, User> userRepo;
+        private IAuthenticationService _authenticationService;
 
         [SetUp]
         public void Setup()
         {
-            
-
             DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder().UseInMemoryDatabase("dummyDB");
             _context = new HotelBookingSystemContext(optionsBuilder.Options);
             userRepo = new UserRepository(_context);
+            _authenticationService = new AuthenticationService();
         }
 
         [Test]
         public async Task AddUserPassTest()
         {
+            HMACSHA512 hMACSHA512 = new HMACSHA512();
+
             User user = await userRepo.Add(new User
             {
-
+                Email = "bhavan@gmail.com",
+                Role = "guest",
+                PasswordHashKey = _authenticationService.GetHashKey(hMACSHA512),
+                HashedPassword = _authenticationService.GetHashedPassword(hMACSHA512, "password")
             });
+
+            Assert.IsNotNull(user.Id);
+        }
+
+        [Test]
+        public async Task GetUserByKeyPassTest()
+        {
+            // add one user
+            HMACSHA512 hMACSHA512 = new HMACSHA512();
+
+            User newUser = await userRepo.Add(new User
+            {
+                Email = "john@gmail.com",
+                Role = "guest",
+                PasswordHashKey = _authenticationService.GetHashKey(hMACSHA512),
+                HashedPassword = _authenticationService.GetHashedPassword(hMACSHA512, "password")
+            });
+
+            User user = await userRepo.GetByKey(newUser.Id);
+
+            Assert.IsNotNull(user);
+        }
+
+        [Test]
+        public void GetUserByKeyFailTest ()
+        {
+            var ex = Assert.ThrowsAsync<UserNotFoundException>(async () => await userRepo.GetByKey(98));
+
+            Assert.That(ex.Message, Is.EqualTo($"No user was found with this id {98}"));
+        }
+
+        [Test]
+        public async Task UpdateUserPassTest ()
+        {
+            HMACSHA512 hMACSHA512 = new HMACSHA512();
+
+            User user = await userRepo.Add(new User
+            {
+                Email = "bhavan@gmail.com",
+                Role = "guest",
+                PasswordHashKey = _authenticationService.GetHashKey(hMACSHA512),
+                HashedPassword = _authenticationService.GetHashedPassword(hMACSHA512, "password")
+            });
+
+            user.Email = "bhavankumar@gmail.com";
+
+            User updatedUser = await userRepo.Update(user);
+
+            Assert.That(updatedUser.Email, Is.EqualTo("bhavankumar@gmail.com"));
+        }
+
+        [Test]
+        public async Task UpdateUserFailTest ()
+        {
+            HMACSHA512 hMACSHA512 = new HMACSHA512();
+
+            User user = new User
+            {
+                Id = 100,
+                Email = "bhavan@gmail.com",
+                Role = "guest",
+                PasswordHashKey = _authenticationService.GetHashKey(hMACSHA512),
+                HashedPassword = _authenticationService.GetHashedPassword(hMACSHA512, "password")
+            };
+
+            var ex = Assert.ThrowsAsync<UserNotFoundException>(async () => await userRepo.Update(user));
+
+            Assert.That(ex.Message, Is.EqualTo($"No user was found with this id {100}"));
+        }
+
+        [Test]
+        public async Task DeleteUserPassTest()
+        {
+            HMACSHA512 hMACSHA512 = new HMACSHA512();
+
+            User user = await userRepo.Add(new User
+            {
+                Email = "bhavan@gmail.com",
+                Role = "guest",
+                PasswordHashKey = _authenticationService.GetHashKey(hMACSHA512),
+                HashedPassword = _authenticationService.GetHashedPassword(hMACSHA512, "password")
+            });
+
+            int userId = user.Id;
+
+            await userRepo.Delete(userId);
+
+            var ex = Assert.ThrowsAsync<UserNotFoundException>(async () => await userRepo.GetByKey(userId));
+
+            Assert.That(ex.Message, Is.EqualTo($"No user was found with this id {userId}"));
+        }
+
+        [Test]
+        public async Task DeleteUserFailTest()
+        {
+            var ex = Assert.ThrowsAsync<UserNotFoundException>(async () => await userRepo.Delete(100));
+
+            Assert.That(ex.Message, Is.EqualTo($"No user was found with this id {100}"));
         }
     }
 }
--- HotelBookingSystemSolution/APITest/obj/Debug/net6.0/APITest.AssemblyInfo.cs
+++ HotelBookingSystemSolution/APITest/obj/Debug/net6.0/APITest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APITest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+125475f0108b7138aeaf9cf7fbf264dd4e79287b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66e0176a38c9afff1891490664e662b3b7097514")]
 [assembly: System.Reflection.AssemblyProductAttribute("APITest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APITest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
